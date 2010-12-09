1f8d610e0d48dde22970dd8c4fab403d7d92527c
Emerson Vinicius <talk@evinicius.com>
2010-12-09 02:52:37 -0200

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,25 @@
+lib_dir = File.expand_path('../../lib', __FILE__)
+$:.unshift lib_dir unless $:.include?(lib_dir)
+
 require 'rubygems'
+require 'sinatra'
 require 'spec'
 require 'spec/autorun'
 require 'spec/interop/test'
 require 'rack/test'
 require 'haml'
 
-lib_dir = File.expand_path('../../lib', __FILE__)
+set :environment, :test
+set :run, false
+set :raise_errors, true
+set :logging, false
 
-$:.unshift lib_dir unless $:.include?(lib_dir)
+module HelperMethods
+  def fixture(id)
+    "#{File.read(File.dirname(__FILE__) + "/fixtures/articles/#{id}.html")}"
+  end
+end
 
+Spec::Runner.configure do |config|
+  config.include(HelperMethods) 
+end
